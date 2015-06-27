f3451a3425d7c435429d9539dd0d5fccf6b2305b
pulkitrast <pulkitrast@gmail.com>
2015-06-27 18:40:03 +0530

    made basic ui changes

---
+++ mobile/src/main/java/org/openhab/habdroid/core/HABDroid.java
@@ -0,0 +1,41 @@
+package org.openhab.habdroid.core;
+
+import android.app.Activity;
+import android.app.Application;
+import android.util.Log;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.HashMap;
+
+/**
+ * Created by belovictor on 26/01/15.
+ */
+/*
+public class HABDroid extends Application {
+
+    public enum TrackerName {
+        APP_TRACKER // Tracker used only in this app.
+    }
+    private HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+    private static final String PROPERTY_ID = "UA-39285202-1";
+    private static final String TAG = "HABDroid";
+
+    public synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+            Log.d(TAG, "GoogleAnalytics.getInstance");
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            analytics.setDryRun(false);
+            Log.d(TAG, "newTracker");
+            Tracker t = analytics.newTracker(PROPERTY_ID);
+
+            Log.d(TAG, "Tracker done");
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
+
+}
+/*/
